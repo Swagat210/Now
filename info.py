7eff08a22e6cd28f98cffd6aefa810a13df113ab
Swagat210 <swagatkumar210@gmail.com>
2025-09-29 23:14:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ LIMIT_MODE = is_enabled((environ.get('LIMIT_MODE', 'False')), False)
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5694062287 7388366658').split()]
 OWNER_USER_NAME = environ.get("OWNER_USER_NAME", "RockSrkarmy") # widout 👉 @
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '3020581103).split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '3020581103').split()]
 # post channel auto post new movie
 POST_CHANNELS = list(map(int, (channel.strip() for channel in environ.get('POST_CHANNELS', '3020581103').split(','))))
 AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '0'))
